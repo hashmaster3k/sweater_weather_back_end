d687ff291514489623964917919e0bf55052054b
Hashim Gari <hashmaster3k@gmail.com>
2020-11-07 10:45:29 -0700

    update controller for map facade

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,3 +1,5 @@
 class Api::V1::WeatherController < ApplicationController
-  def index; end
+  def index
+    MapFacade.coordinates_for_location(params[:location])
+  end
 end
--- spec/requests/api/v1/weather_request_spec.rb
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Weather API' do
   describe 'happy paths' do
     it 'has a proper route, controller and action' do
+      location = 'denver,co'
+
+      body = File.read('spec/fixtures/map_data_denver.json')
+      stub_request(:get, "#{ENV['MAP_URL']}geocoding/v1/address?key=#{ENV['MAP_API_KEY']}&location=#{location}").
+           to_return(status: 200, body: body, headers: {})
+           
       get '/api/v1/forecast?location=denver,co'
     end
   end
