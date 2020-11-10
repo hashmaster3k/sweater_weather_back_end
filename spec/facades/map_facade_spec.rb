3712d8df33c80bd1cdf7be65ce3f96e7d63cf761
Hashim Gari <hashmaster3k@gmail.com>
2020-11-10 15:52:01 -0700

    tests for new methods

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -12,4 +12,42 @@ RSpec.describe MapFacade do
 
     expect(result).to be_a(Coordinate)
   end
+
+  it 'returns array with time travel string and total hours' do
+    origin = 'Denver,CO'
+    destination = 'Pueblo,CO'
+
+    distance_body = File.read('spec/fixtures/route_data_denver_pueblo.json')
+    stub_request(:get, "#{ENV['MAP_URL']}directions/v2/route?from=#{origin}&key=#{ENV['MAP_API_KEY']}&to=#{destination}").
+       to_return(status: 200, body: distance_body, headers: {})
+
+    result = MapFacade.travel_time_between_two_locations(origin, destination)
+
+    expect(result).to be_an(Array)
+    expect(result.count).to eq(2)
+    expect(result.first).to be_a(String)
+    expect(result.last).to be_a(Numeric)
+  end
+
+  it 'returns array with impossible route string and 0 hours' do
+    origin = 'Miami, FL'
+    destination = 'Havana, Cuba'
+
+    error_body = File.read('spec/fixtures/route_error.json')
+    stub_request(:get, "#{ENV['MAP_URL']}directions/v2/route?from=#{origin}&key=#{ENV['MAP_API_KEY']}&to=#{destination}").
+       to_return(status: 200, body: error_body, headers: {})
+
+    result = MapFacade.travel_time_between_two_locations(origin, destination)
+
+    expect(result).to be_an(Array)
+    expect(result.count).to eq(2)
+    expect(result.first).to be_a(String)
+    expect(result.last).to be_a(Numeric)
+  end
+
+  it 'can convert seconds to string time' do
+    expect(MapFacade.send(:convert_to_readable_time, 923823)).to eq("10 day 16 hr 37 min")
+    expect(MapFacade.send(:convert_to_readable_time, 9238)).to eq("2 hr 33 min")
+    expect(MapFacade.send(:convert_to_readable_time, 923)).to eq("15 min")
+  end
 end
